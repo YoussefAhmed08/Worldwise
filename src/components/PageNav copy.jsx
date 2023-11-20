a9fe771af8fe374ad438aa06b8b58784d0f53389
Youssef Ahmed <120047710+YoussefAhmed08@users.noreply.github.com>
2023-11-20 13:53:53 +0200

    Add files via upload

---
+++ src/components/PageNav copy.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";  
+
+function PageNav() {
+  return (
+    <div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <NavLink to="/pricing">Pricing</NavLink>
+          </li>
+          <li>
+            <NavLink to="/product">Product</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default PageNav;
